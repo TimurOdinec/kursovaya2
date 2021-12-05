5e5ac198169d4a39f9829ff81af792f84d25cea9
OdinetsDmitry <User@DESKTOP-8PU1PQQ>
2021-12-05 04:23:45 +0300

    add

--- Project1Git/MyForm.h
+++ Project1Git/MyForm.h
@@ -73,6 +73,7 @@ namespace Project1Git {
 		private: System::Windows::Forms::ToolStripMenuItem^ p261ToolStripMenuItem;
 		private: System::Windows::Forms::ToolStripMenuItem^ p262ToolStripMenuItem;
 		private: System::Windows::Forms::ToolStripMenuItem^ p263ToolStripMenuItem;
+		private: System::Windows::Forms::PictureBox^ pictureBox1;
 
 		private:
 			/// <summary>
@@ -87,6 +88,7 @@ namespace Project1Git {
 			/// </summary>
 			void InitializeComponent(void)
 			{
+				System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 				this->button3 = (gcnew System::Windows::Forms::Button());
 				this->label1 = (gcnew System::Windows::Forms::Label());
 				this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
@@ -111,13 +113,15 @@ namespace Project1Git {
 				this->p253ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 				this->p254ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 				this->p26ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
-				this->p3ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
-				this->p4ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 				this->p261ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 				this->p262ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 				this->p263ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+				this->p3ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+				this->p4ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+				this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 				(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 				this->menuStrip1->SuspendLayout();
+				(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 				this->SuspendLayout();
 				// 
 				// button3
@@ -133,7 +137,7 @@ namespace Project1Git {
 				// label1
 				// 
 				this->label1->AutoSize = true;
-				this->label1->Location = System::Drawing::Point(34, 101);
+				this->label1->Location = System::Drawing::Point(44, 144);
 				this->label1->Name = L"label1";
 				this->label1->Size = System::Drawing::Size(46, 17);
 				this->label1->TabIndex = 5;
@@ -179,7 +183,7 @@ namespace Project1Git {
 						this->p12ToolStripMenuItem, this->p13ToolStripMenuItem, this->p14ToolStripMenuItem, this->p15ToolStripMenuItem
 				});
 				this->p1ToolStripMenuItem->Name = L"p1ToolStripMenuItem";
-				this->p1ToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+				this->p1ToolStripMenuItem->Size = System::Drawing::Size(109, 26);
 				this->p1ToolStripMenuItem->Text = L"p1";
 				// 
 				// p11ToolStripMenuItem
@@ -241,34 +245,34 @@ namespace Project1Git {
 						this->p22ToolStripMenuItem, this->p23ToolStripMenuItem, this->p24ToolStripMenuItem, this->p25ToolStripMenuItem, this->p26ToolStripMenuItem
 				});
 				this->p2ToolStripMenuItem->Name = L"p2ToolStripMenuItem";
-				this->p2ToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+				this->p2ToolStripMenuItem->Size = System::Drawing::Size(109, 26);
 				this->p2ToolStripMenuItem->Text = L"p2";
 				// 
 				// p21ToolStripMenuItem
 				// 
 				this->p21ToolStripMenuItem->Name = L"p21ToolStripMenuItem";
-				this->p21ToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+				this->p21ToolStripMenuItem->Size = System::Drawing::Size(117, 26);
 				this->p21ToolStripMenuItem->Text = L"p21";
 				this->p21ToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::p21ToolStripMenuItem_Click);
 				// 
 				// p22ToolStripMenuItem
 				// 
 				this->p22ToolStripMenuItem->Name = L"p22ToolStripMenuItem";
-				this->p22ToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+				this->p22ToolStripMenuItem->Size = System::Drawing::Size(117, 26);
 				this->p22ToolStripMenuItem->Text = L"p22";
 				this->p22ToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::p22ToolStripMenuItem_Click);
 				// 
 				// p23ToolStripMenuItem
 				// 
 				this->p23ToolStripMenuItem->Name = L"p23ToolStripMenuItem";
-				this->p23ToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+				this->p23ToolStripMenuItem->Size = System::Drawing::Size(117, 26);
 				this->p23ToolStripMenuItem->Text = L"p23";
 				this->p23ToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::p23ToolStripMenuItem_Click);
 				// 
 				// p24ToolStripMenuItem
 				// 
 				this->p24ToolStripMenuItem->Name = L"p24ToolStripMenuItem";
-				this->p24ToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+				this->p24ToolStripMenuItem->Size = System::Drawing::Size(117, 26);
 				this->p24ToolStripMenuItem->Text = L"p24";
 				this->p24ToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::p24ToolStripMenuItem_Click);
 				// 
@@ -279,34 +283,34 @@ namespace Project1Git {
 						this->p252ToolStripMenuItem, this->p253ToolStripMenuItem, this->p254ToolStripMenuItem
 				});
 				this->p25ToolStripMenuItem->Name = L"p25ToolStripMenuItem";
-				this->p25ToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+				this->p25ToolStripMenuItem->Size = System::Drawing::Size(117, 26);
 				this->p25ToolStripMenuItem->Text = L"p25";
 				// 
 				// p251ToolStripMenuItem
 				// 
 				this->p251ToolStripMenuItem->Name = L"p251ToolStripMenuItem";
-				this->p251ToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+				this->p251ToolStripMenuItem->Size = System::Drawing::Size(125, 26);
 				this->p251ToolStripMenuItem->Text = L"p251";
 				this->p251ToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::p251ToolStripMenuItem_Click);
 				// 
 				// p252ToolStripMenuItem
 				// 
 				this->p252ToolStripMenuItem->Name = L"p252ToolStripMenuItem";
-				this->p252ToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+				this->p252ToolStripMenuItem->Size = System::Drawing::Size(125, 26);
 				this->p252ToolStripMenuItem->Text = L"p252";
 				this->p252ToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::p252ToolStripMenuItem_Click);
 				// 
 				// p253ToolStripMenuItem
 				// 
 				this->p253ToolStripMenuItem->Name = L"p253ToolStripMenuItem";
-				this->p253ToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+				this->p253ToolStripMenuItem->Size = System::Drawing::Size(125, 26);
 				this->p253ToolStripMenuItem->Text = L"p253";
 				this->p253ToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::p253ToolStripMenuItem_Click);
 				// 
 				// p254ToolStripMenuItem
 				// 
 				this->p254ToolStripMenuItem->Name = L"p254ToolStripMenuItem";
-				this->p254ToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+				this->p254ToolStripMenuItem->Size = System::Drawing::Size(125, 26);
 				this->p254ToolStripMenuItem->Text = L"p254";
 				this->p254ToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::p254ToolStripMenuItem_Click);
 				// 
@@ -317,49 +321,60 @@ namespace Project1Git {
 						this->p262ToolStripMenuItem, this->p263ToolStripMenuItem
 				});
 				this->p26ToolStripMenuItem->Name = L"p26ToolStripMenuItem";
-				this->p26ToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+				this->p26ToolStripMenuItem->Size = System::Drawing::Size(117, 26);
 				this->p26ToolStripMenuItem->Text = L"p26";
-				// 
-				// p3ToolStripMenuItem
-				// 
-				this->p3ToolStripMenuItem->Name = L"p3ToolStripMenuItem";
-				this->p3ToolStripMenuItem->Size = System::Drawing::Size(224, 26);
-				this->p3ToolStripMenuItem->Text = L"p3";
-				this->p3ToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::p3ToolStripMenuItem_Click);
-				// 
-				// p4ToolStripMenuItem
-				// 
-				this->p4ToolStripMenuItem->Name = L"p4ToolStripMenuItem";
-				this->p4ToolStripMenuItem->Size = System::Drawing::Size(224, 26);
-				this->p4ToolStripMenuItem->Text = L"p4";
-				this->p4ToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::p4ToolStripMenuItem_Click);
 				// 
 				// p261ToolStripMenuItem
 				// 
 				this->p261ToolStripMenuItem->Name = L"p261ToolStripMenuItem";
-				this->p261ToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+				this->p261ToolStripMenuItem->Size = System::Drawing::Size(125, 26);
 				this->p261ToolStripMenuItem->Text = L"p261";
 				this->p261ToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::p261ToolStripMenuItem_Click);
 				// 
 				// p262ToolStripMenuItem
 				// 
 				this->p262ToolStripMenuItem->Name = L"p262ToolStripMenuItem";
-				this->p262ToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+				this->p262ToolStripMenuItem->Size = System::Drawing::Size(125, 26);
 				this->p262ToolStripMenuItem->Text = L"p262";
 				this->p262ToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::p262ToolStripMenuItem_Click);
 				// 
 				// p263ToolStripMenuItem
 				// 
 				this->p263ToolStripMenuItem->Name = L"p263ToolStripMenuItem";
-				this->p263ToolStripMenuItem->Size = System::Drawing::Size(224, 26);
+				this->p263ToolStripMenuItem->Size = System::Drawing::Size(125, 26);
 				this->p263ToolStripMenuItem->Text = L"p263";
 				this->p263ToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::p263ToolStripMenuItem_Click);
+				// 
+				// p3ToolStripMenuItem
+				// 
+				this->p3ToolStripMenuItem->Name = L"p3ToolStripMenuItem";
+				this->p3ToolStripMenuItem->Size = System::Drawing::Size(109, 26);
+				this->p3ToolStripMenuItem->Text = L"p3";
+				this->p3ToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::p3ToolStripMenuItem_Click);
+				// 
+				// p4ToolStripMenuItem
+				// 
+				this->p4ToolStripMenuItem->Name = L"p4ToolStripMenuItem";
+				this->p4ToolStripMenuItem->Size = System::Drawing::Size(109, 26);
+				this->p4ToolStripMenuItem->Text = L"p4";
+				this->p4ToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::p4ToolStripMenuItem_Click);
+				// 
+				// pictureBox1
+				// 
+				this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+				this->pictureBox1->Location = System::Drawing::Point(738, 31);
+				this->pictureBox1->Name = L"pictureBox1";
+				this->pictureBox1->Size = System::Drawing::Size(361, 130);
+				this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+				this->pictureBox1->TabIndex = 8;
+				this->pictureBox1->TabStop = false;
 				// 
 				// MyForm
 				// 
 				this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 				this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 				this->ClientSize = System::Drawing::Size(1135, 544);
+				this->Controls->Add(this->pictureBox1);
 				this->Controls->Add(this->dataGridView1);
 				this->Controls->Add(this->label1);
 				this->Controls->Add(this->button3);
@@ -371,6 +386,7 @@ namespace Project1Git {
 				(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 				this->menuStrip1->ResumeLayout(false);
 				this->menuStrip1->PerformLayout();
+				(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 				this->ResumeLayout(false);
 				this->PerformLayout();
 
@@ -386,6 +402,8 @@ namespace Project1Git {
 				this->Text = "јвтопарк";						//название формы
 				this->button3->Text = "Exit";					//название кнопки
 				this->dataGridView1->Visible = false;			//пр€чем твблицу
+				//this->pictureBox1->Load(L"D:\\ADZINETS\\ODM_C++\\docs\\docsKursWork2\\20210906kurs\\usedInformation\\logo.jpg");
+				//this->pictureBox1->Load(L"logo.jpg");
 				//создание новой формы авторизации
 				Project1Git::MyForm1^ myForm1 = gcnew Project1Git::MyForm1();
 				myForm1->ShowDialog();	//передача фокуса форме авторизации
